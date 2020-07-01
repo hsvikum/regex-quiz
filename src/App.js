626836e7a90ec8229277e86569888961853751e0
Shalitha Vikum <hsvikum@gmail.com>
2020-07-01 15:55:58 +0530

    moved from primeface to bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import './styles/App.css';
-import 'primereact/resources/themes/nova-colored/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Welcome from './components/welcome';
 import Quiz from './components/quiz';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
-          <Switch>
-            <Route exact path="/" component={Welcome}/>
-            <Route path="/quiz" component={Quiz}/>
-          </Switch>
+        <Container>
+            <Switch>
+              <Route exact path="/" component={Welcome}/>
+              <Route path="/quiz" component={Quiz}/>
+            </Switch>
+          </Container>
         </div>
     </BrowserRouter>
   );
