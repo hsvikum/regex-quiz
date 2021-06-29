4e3de9312c9af2fc682930f2386c217a07b051db
hsvikum <hsvikum@gmail.com>
2021-06-29 17:22:20 +0530

    Trying out HashRouter

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import Welcome from './components/welcome';
 import Quiz from './components/quiz';
 import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
         <div className="App">
-        <Container>
+          <Container>
             <Switch>
               <Route exact path="/" component={Welcome}/>
               <Route path="/quiz" component={Quiz}/>
             </Switch>
           </Container>
         </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
